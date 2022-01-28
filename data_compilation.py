3dfe96cb6a1c7f05e668325f04492e05579f58c6
VitaL <vitaldukm@gmail.com>
2022-01-28 20:21:55 +0000

    Update data_compilation.py

--- data_compilation.py
+++ data_compilation.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import glob
 import config
-import numpy as np
 
 
 # Combine multiple csv into one dataframe
 
-path = r'C:\Users\murci\Documents\GitHub\resi-property-webscrape\csv' # use your path
+path = r'C:\Users\murci\Documents\GitHub\resi-property-webscrape\csv'
 all_files = glob.glob(path + "/*.csv")
 
 li = []
@@ -18,15 +17,16 @@ for filename in all_files:
 frame = pd.concat(li, axis=0, ignore_index=True)
 
 
-
 # Clean the data and remove duplicate URL's
 
 df = frame.drop_duplicates(subset=['URL'], keep=False).copy()
 print(len(frame))
 
+# Redirect the URL's
 
 df['URL'] = df.URL.replace({config.zp: "https://www.zoopla.co.uk/for-sale/details/", config.otm: 'https://www.onthemarket.com/details/', config.rm:"https://www.rightmove.co.uk/properties/"}, regex=True)
 
 
+# Save to csv
 
 df.to_csv("testing.csv")
